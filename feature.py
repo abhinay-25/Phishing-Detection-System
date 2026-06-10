c37632b5e2389d1ffa8eed09cab6068178979fc3
abhinay-25 <palleabhinay462@gmail.com>
2026-06-10 12:35:00 +0530

    msg

--- feature.py
+++ feature.py
@@ -11,7 +11,7 @@ import time
 from dateutil.parser import parse as date_parse
 from urllib.parse import urlparse    
 
-class FeatureExtraction:
+class FeatureExtraction:    
     features = []
     def __init__(self,url):
         self.features = []
